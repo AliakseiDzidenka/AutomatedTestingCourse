5741d5c85e3dd192a325f6e7acc4466e88791c1b
alexd <alexdidenko7@gmail.com>
2022-05-12 23:22:19 +0300

    Completed task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Enter string: ");
+string? str = Console.ReadLine();
+if (str == null)
+{
+    Console.WriteLine("empty");
+    return;
+}
+int maxCount = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    int letterCounter = 1;
+    for (int j = i; j < str.Length - 1; j++)
+    {
+        if (str[j] != str[j + 1])
+        {
+            letterCounter++;
+        }
+        else
+        {
+            i = j;
+            if (letterCounter > maxCount)
+            {
+                maxCount = letterCounter;
+            }
+            break;
+        }
+        if (j == str.Length - 2)
+        {
+            if (letterCounter > maxCount)
+            {
+                maxCount = letterCounter;
+            }
+        }
+    }
+}
+Console.WriteLine("Maximum number of unequal consecutive characters in a string: " + maxCount.ToString());
